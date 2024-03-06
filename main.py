34d32909ebb056228a4b80668b0e34d27c0c4b32
Grzegorz Ciepiel <grzegorz@ciepiel.pl>
2024-03-06 14:30:52 +0100

    add sql analyse

--- main.py
+++ main.py
@@ -25,3 +25,36 @@ clickstream = spark.read.option('delimiter', '\t').option('header', True).option
 
 # Display the DataFrame to the notebook
 clickstream.show()
+
+
+# Drop target columns
+clickstream = clickstream.drop('language_code')
+
+# Display the first few rows of the DataFrame
+clickstream.show(3)
+# Display the new schema in the notebook
+clickstream.printSchema()
+
+
+# Rename `referrer` and `resource` to `source_page` and `target_page`
+clickstream = clickstream.withColumnRenamed('referrer', 'source_page')
+clickstream = clickstream.withColumnRenamed('resource', 'target_page')
+  
+# Display the first few rows of the DataFrame
+clickstream.show(3)
+# Display the new schema in the notebook
+clickstream.printSchema()
+
+
+# Filter and sort the DataFrame using PySpark DataFrame method
+clickstream.select([ 'source_page', 'target_page','link_category', 'click_count']).filter('target_page == "Hanging_Gardens_of_Babylon"').orderBy('click_count').show()
+
+
+# Filter and sort the DataFrame using SQL
+q = '''SELECT * FROM clickstream WHERE target_page = "Hanging_Gardens_of_Babylon"
+ORDER BY click_count;'''
+
+spark.sql(q).show()
+
+
+
